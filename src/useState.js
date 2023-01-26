86e46f3f4d507ac739f476624f19c3295f28ab5f
SHIHANSYED <shihansyedali@gmail.com>
2023-01-26 19:32:27 +0530

    usestate

---
+++ src/useState.js
@@ -0,0 +1,23 @@
+
+import { useState } from 'react';
+
+function Names(){
+    const[name,setName]=useState(["raju","radha","mayavi"])
+    const[list,setList]=useState("")
+    const addName=()=>{
+      setName([...name,list])
+    }
+    return(
+      <div>
+        <ul>
+          {name.map((name)=>(
+            <li>{name}</li>
+          ))}
+        </ul>
+        <input type="text" value={list} onChange={(e)=>{setList(e.target.value)}}/>
+        <button onClick={addName}> Add Name</button>
+      </div>
+    )
+  }
+
+  export default Names;
